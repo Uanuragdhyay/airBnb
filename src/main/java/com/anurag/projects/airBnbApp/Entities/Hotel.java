d7289a6f785c8da11e64a01ccd57d38ef2b51b9a
Anurag <uanurag216@gmail.com>
2025-10-30 11:46:26 +0530

    error removed

--- src/main/java/com/anurag/projects/airBnbApp/Entities/Hotel.java
+++ src/main/java/com/anurag/projects/airBnbApp/Entities/Hotel.java
@@ -11,7 +11,7 @@ import java.time.LocalDateTime;
 @Entity
 @Getter
 @Setter
-@Table(name = "hotel")
+@Table(name = "hotels")
 public class Hotel {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
